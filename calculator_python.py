57f44a1f163c01931ae9e30ec3bd76b60d1b5aa2
sachin <sachinswatz1798@gmail.com>
2025-08-07 18:05:18 +0530

    Completed till calculator textbox

--- calculator_python.py
+++ calculator_python.py
@@ -1,1 +1,26 @@
+import tkinter as tk#tkinter modules as tk anothername
+from tkinter import *#all the libraries,modules are imported
+import tkinter.messagebox#seperate submodule messagebox gives functions for giving various user messages
+from tkinter.constants import SUNKEN#import a specific constant
 
+win = tk.Tk()#create the main window of application
+win.title('Calculator')#given the title of window
+
+frame = tk.Frame(win, bg="skyblue", padx=10)
+frame.pack()#create a frame widget with skyblue colour above main window
+
+entry = tk.Entry(frame, relief=SUNKEN, borderwidth=3, width=30)#Entry widget placed inside frame widget,a single line text input field
+entry.grid(row=0, column=0, columnspan=3, ipady=2, pady=2)
+
+def click(num):
+    entry.insert(tk.END, num)
+
+def equal():
+    try:
+        res = str(eval(entry.get()))
+        entry.delete(0, tk.END)
+        entry.insert(0, res)
+    except:
+        tk.messagebox.showinfo("Error", "Syntax Error")
+
+win.mainloop()  
